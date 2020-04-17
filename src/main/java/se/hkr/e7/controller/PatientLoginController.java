8bcfa88e2a80d573fe10fc8e2279ece0ae608d67
mnq11 <57759177+mnq11@users.noreply.github.com>
2020-04-17 18:58:21 +0200

    clean up rename.

---
+++ src/main/java/se/hkr/e7/controller/PatientLoginController.java
@@ -0,0 +1,64 @@
+package se.hkr.e7.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.hibernate.query.Query;
+import se.hkr.e7.Patient;
+import se.hkr.e7.SQL;
+import se.hkr.e7.Singleton;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class PatientLoginController {
+
+
+    public TextField ssnText;
+
+
+    public void Back(ActionEvent actionEvent) throws IOException {
+
+        Node node = (Node) actionEvent.getSource();
+        Scene currScene = node.getScene();
+        Stage stage = (Stage) currScene.getWindow();
+        URL resource = getClass().getClassLoader().getResource("login.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+    public void Cancel(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void patientLogin(ActionEvent actionEvent) throws IOException {
+
+
+        //this will set the primary key of tha patient to singleton .
+        Singleton.getInstance().setSsn(ssnText.getText());
+
+
+//        moving to the Dashboard of the patient ....
+
+        Node node = (Node) actionEvent.getSource();
+        Scene currScene = node.getScene();
+        Stage stage = (Stage) currScene.getWindow();
+        URL resource = getClass().getClassLoader().getResource("patientDashboard.fxml");
+        assert resource != null;
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
+
--- src/main/java/se/hkr/e7/controller/WelcomeController.java
+++ src/main/java/se/hkr/e7/controller/WelcomeController.java
@@ -1,32 +1,23 @@
-package Controller;
+package se.hkr.e7.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.hibernate.query.Query;
-import se.hkr.e7.Patient;
-import se.hkr.e7.SQL;
-import se.hkr.e7.Singleton;
-
 import java.io.IOException;
 import java.net.URL;
 
-public class PatientLoginController {
+public class WelcomeController {
 
 
-    public TextField ssnText;
 
-
-    public void Back(ActionEvent actionEvent) throws IOException {
-
+    public void StaffLogin(ActionEvent actionEvent) throws IOException {
         Node node = (Node) actionEvent.getSource();
         Scene currScene = node.getScene();
         Stage stage = (Stage) currScene.getWindow();
-        URL resource = getClass().getClassLoader().getResource("login.fxml");
+        URL resource = getClass().getClassLoader().getResource("StaffLogin.fxml");
         Parent root = FXMLLoader.load(resource);
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -34,25 +25,26 @@ public class PatientLoginController {
 
 
 
+
     }
 
-    public void Cancel(ActionEvent actionEvent) {
+    public void PatientLogin(ActionEvent actionEvent) throws IOException {
+
+        Node node = (Node) actionEvent.getSource();
+        Scene currScene = node.getScene();
+        Stage stage = (Stage) currScene.getWindow();
+        URL resource = getClass().getClassLoader().getResource("patientLogin.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+
+    public void cancel(ActionEvent actionEvent) {
         System.exit(0);
     }
-
-    public void patientLogin(ActionEvent actionEvent) throws IOException {
-
-        Singleton.getInstance().setSsn(ssnText.getText());
-        Node node = (Node) actionEvent.getSource();
-        Scene currScene = node.getScene();
-        Stage stage = (Stage) currScene.getWindow();
-        URL resource = getClass().getClassLoader().getResource("patientDashboard.fxml");
-        Parent root = FXMLLoader.load(resource);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-
-    }
 }
-
