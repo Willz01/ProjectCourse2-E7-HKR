ed2d34edbfcc89cd4aeba17e3d96fc6614aece9a
mnq11 <57759177+mnq11@users.noreply.github.com>
2020-04-21 17:45:05 +0200

    rename cancel button to exit

--- src/main/java/se/hkr/e7/controller/WelcomeController.java
+++ src/main/java/se/hkr/e7/controller/WelcomeController.java
@@ -5,11 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
 public class WelcomeController {
+
 
 
 
