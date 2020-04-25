72f2d9a7187203abec8473a9899906df47824294
Philip Klempin <philip@klempin.se>
2020-04-25 23:34:04 +0200

    Refactor AddAdminController

--- src/main/java/se/hkr/e7/controller/Controller.java
+++ src/main/java/se/hkr/e7/controller/Controller.java
@@ -1,10 +1,13 @@
 package se.hkr.e7.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,14 +15,26 @@ import java.net.URL;
 
 public abstract class Controller {
 
-    public void loadScene(String url, ActionEvent actionEvent) throws IOException {
-        Node node = (Node) actionEvent.getSource();
-        Scene currScene = node.getScene();
-        Stage stage = (Stage) currScene.getWindow();
-        URL resource = getClass().getClassLoader().getResource(String.valueOf(url));
-        Parent root = FXMLLoader.load(resource);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    public void loadScene(String name, ActionEvent actionEvent) {
+        try {
+            Node node = (Node) actionEvent.getSource();
+            Scene currScene = node.getScene();
+            Stage stage = (Stage) currScene.getWindow();
+            URL resource = getClass().getClassLoader().getResource(name);
+            Parent root = FXMLLoader.load(resource);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+            if (!name.equals("view/Welcome.fxml")) {
+                System.out.println("not");
+                loadScene("view/Welcome.fxml", actionEvent);
+            } else {
+                new Alert(Alert.AlertType.ERROR, "There was an error loading the default scene.", ButtonType.CLOSE)
+                        .showAndWait();
+                System.exit(1);
+            }
+        }
     }
 }
