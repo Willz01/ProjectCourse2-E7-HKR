9eea7da33f7a051dabba8240f02a7adf00e9ba68
Hani311 <hani_adel.alzir0072@stud.hkr.se>
2020-04-25 17:07:34 +0200

    Add pop up error message

--- src/main/java/se/hkr/e7/controller/StaffLoginController.java
+++ src/main/java/se/hkr/e7/controller/StaffLoginController.java
@@ -1,6 +1,7 @@
 package se.hkr.e7.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import se.hkr.e7.model.DatabaseHandler;
@@ -20,7 +21,7 @@ public class StaffLoginController extends Controller {
         passwordCheck.setText(null);
         error1.setText(null);
         if (passwordText.getText().equals("") || ssnText.getText().equals("")) {
-            error1.setText("fields can not be empty ");
+            showError(new Exception("fields can not be empty "));
         } else {
             try {
                 Employee employee = DatabaseHandler.load(Employee.class, ssnText.getText());
@@ -32,7 +33,7 @@ public class StaffLoginController extends Controller {
                 }
 
                 if (employee.getSsn() != null && !employee.checkPassword(passwordText.getText())) {
-                    passwordCheck.setText("wrong password ");
+                    showError(new Exception("wrong password "));
                 }
 
                 if (employee.getRole() == Employee.Role.DOCTOR && employee.checkPassword(passwordText.getText())) {
@@ -43,9 +44,15 @@ public class StaffLoginController extends Controller {
                     loadScene("view/AnalyserDashboard.fxml",actionEvent);
                 }
             } catch (Exception exception) {
-                error1.setText("could not login , please check your password and ssn ");
+                showError(new Exception("could not login , please check your password and ssn "));
             }
         }
+    }
+    private void showError(Exception e) {
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        error.setTitle("input error please retry");
+        error.setContentText(e.getMessage());
+        error.showAndWait();
     }
 
 
