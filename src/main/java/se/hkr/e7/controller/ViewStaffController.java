a775c04c25f76e63f94798e7d9753ec5603f05d6
mnq11 <57759177+mnq11@users.noreply.github.com>
2020-04-25 02:23:18 +0200

    show staff basic

--- src/main/java/se/hkr/e7/controller/ViewStaffController.java
+++ src/main/java/se/hkr/e7/controller/ViewStaffController.java
@@ -1,16 +1,24 @@
 package se.hkr.e7.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import se.hkr.e7.model.DatabaseHandler;
+import se.hkr.e7.model.Employee;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
+import static se.hkr.e7.model.DatabaseHandler.loadAllData;
 
 public class ViewStaffController {
+    public TextArea showStaff;
     public void Back(ActionEvent actionEvent) throws IOException {
         Node node = (Node) actionEvent.getSource();
         Scene currScene = node.getScene();
@@ -22,7 +30,12 @@ public class ViewStaffController {
         stage.show();
     }
 
+    @FXML
+    public void initialize() throws IOException {
+        List<Employee> employees = loadAllData(Employee.class, DatabaseHandler.getSession());
+        showStaff.setText(String.valueOf(employees));
 
+    }
 
     public void Exit() {
         System.exit(0);
