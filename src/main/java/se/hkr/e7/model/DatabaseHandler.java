482d1ad6b651878ba54f419d220462bddf686e90
Philip Klempin <philip@klempin.se>
2020-04-25 21:13:23 +0200

    Move annotated class definitions to DatabaseHandler

--- src/main/java/se/hkr/e7/model/DatabaseHandler.java
+++ src/main/java/se/hkr/e7/model/DatabaseHandler.java
@@ -2,7 +2,11 @@ package se.hkr.e7.model;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -15,7 +19,18 @@ public class DatabaseHandler {
     private static final Session session;
 
     static {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
+        ServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .configure("./hibernate.cfg.xml")
+                .build();
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Patient.class)
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Result.class)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+        sessionFactory = metadata.buildSessionFactory();
         session = sessionFactory.openSession();
     }
 
