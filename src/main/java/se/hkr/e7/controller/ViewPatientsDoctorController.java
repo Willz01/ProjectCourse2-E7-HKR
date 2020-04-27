ea18a13f68f989bdc437da47eb55c5ffbc1763eb
mnq11 <57759177+mnq11@users.noreply.github.com>
2020-04-27 03:50:11 +0200

    Add view patients for the doctors

---
+++ src/main/java/se/hkr/e7/controller/ViewPatientsDoctorController.java
@@ -0,0 +1,33 @@
+package se.hkr.e7.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import se.hkr.e7.model.DatabaseHandler;
+import se.hkr.e7.model.Patient;
+
+import java.util.List;
+
+public class ViewPatientsDoctorController extends Controller {
+
+    public Button exitButton;
+    public Button backButton;
+    public ChoiceBox<String> choiceBox;
+
+    @FXML
+    public void initialize() {
+        exitButton.setOnAction(this::exit);
+        backButton.setOnAction(actionEvent -> loadScene("view/DoctorDashboard.fxml", actionEvent));
+
+        List<Patient> patients = DatabaseHandler.loadAll(Patient.class);
+        int counter = 0;
+
+        for (Patient patient : patients) {
+            counter++;
+            choiceBox.getItems().add(counter + "- Name : " + patient.getName() + " ----  " + " Email : " + patient.getEmail() + " ----  " + " Phone : " + patient.getPhone() + "  ---- " + " SSN: " + patient.getSsn());
+        }
+
+        choiceBox.getItems().add("View Patient");
+        choiceBox.setValue("View Patient");
+    }
+}
