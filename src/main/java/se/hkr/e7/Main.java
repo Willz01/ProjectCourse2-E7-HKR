79890270d2aa3ad1790084eb2de6e3ea8eb0da73
mnq11 <57759177+mnq11@users.noreply.github.com>
2020-04-28 01:11:26 +0200

    .

--- src/main/java/se/hkr/e7/Main.java
+++ src/main/java/se/hkr/e7/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        URL resource = getClass().getClassLoader().getResource("view/DoctorDashboard.fxml");
+        URL resource = getClass().getClassLoader().getResource("view/WelcomeController");
         Parent root = FXMLLoader.load(resource);
         Scene scene = new Scene(root);
         stage.setScene(scene);
