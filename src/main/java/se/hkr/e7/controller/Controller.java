13e67f5c0f793c338ac46c12f01eb0cb35be4312
Philip Klempin <philip@klempin.se>
2020-04-28 12:32:43 +0200

    Cleanup

--- src/main/java/se/hkr/e7/controller/Controller.java
+++ src/main/java/se/hkr/e7/controller/Controller.java
@@ -6,10 +6,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.net.URL;
