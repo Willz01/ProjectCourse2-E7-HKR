c9f1edb94a69cfe7a460577e7d5217172450560b
Hani311 <hani_adel.alzir0072@stud.hkr.se>
2020-04-28 23:31:27 +0200

    Add remove staff function

--- src/main/java/se/hkr/e7/controller/RemoveStaffController.java
+++ src/main/java/se/hkr/e7/controller/RemoveStaffController.java
@@ -1,13 +1,41 @@
 package se.hkr.e7.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import se.hkr.e7.model.DatabaseHandler;
+import se.hkr.e7.model.Employee;
+import se.hkr.e7.model.Location;
 import se.hkr.e7.model.Singleton;
 
 public class RemoveStaffController extends Controller {
 
+    public TextField ssnField;
+    public Label saveLabel;
+    public Button removeButton;
+
     @FXML
     public void initialize() {
         Singleton.getInstance().addSceneHistory("view/RemoveStaff.fxml");
+    }
+
+    public void Remove(ActionEvent actionEvent) {
+        if (ssnField.getText().matches("^([0-9]{2})([0-9]{2})([0-9]{2})([a-zA-Z0-9][0-9]{3})$")) {
+            saveLabel.setText("");
+        }
+        if (!ssnField.getText().matches("^([0-9]{2})([0-9]{2})([0-9]{2})([a-zA-Z0-9][0-9]{3})$")) {
+            saveLabel.setText("input in YYMMDDXXXX form");
+        }
+        Employee employee = DatabaseHandler.load(Employee.class, ssnField.getText());
+
+        try {
+            DatabaseHandler.delete(employee);
+            showConfirmation("Deleted", "Done");
+        } catch (Exception exception) {
+            showError("Did not delete, make sure you have right ssn");
+
+        }
     }
 }
