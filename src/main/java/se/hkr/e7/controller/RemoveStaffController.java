837b0bf0987062366d5728ec862351b1915780f1
Philip Klempin <philip@klempin.se>
2020-04-29 20:45:32 +0200

    Update RemoveStaffController.java

--- src/main/java/se/hkr/e7/controller/RemoveStaffController.java
+++ src/main/java/se/hkr/e7/controller/RemoveStaffController.java
@@ -5,9 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import se.hkr.e7.model.*;
 import se.hkr.e7.model.DatabaseHandler;
 import se.hkr.e7.model.Employee;
+import se.hkr.e7.model.Person;
 import se.hkr.e7.model.Singleton;
 
 public class RemoveStaffController extends Controller {
