1b396b2bc3c4550ae31d9deecc076b2c42dc8d86
Philip Klempin <philip@klempin.se>
2020-04-29 21:41:17 +0200

    Remove DatabaseHandler.reset()

--- src/main/java/se/hkr/e7/Main.java
+++ src/main/java/se/hkr/e7/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import se.hkr.e7.model.DatabaseHandler;
 
 import java.io.IOException;
 import java.net.URL;
@@ -23,6 +22,5 @@ public class Main extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-        DatabaseHandler.reset();
     }
 }
