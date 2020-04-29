22fdaed09d9edc27355693b8529fb593a5ccf05e
Philip Klempin <philip@klempin.se>
2020-04-29 15:35:15 +0200

    Use RemoveAccount to delete either Employee or Patient

--- src/main/java/se/hkr/e7/controller/RemoveAccountController.java
+++ src/main/java/se/hkr/e7/controller/RemoveAccountController.java
@@ -7,7 +7,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import se.hkr.e7.model.DatabaseHandler;
 import se.hkr.e7.model.Employee;
+import se.hkr.e7.model.Patient;
 import se.hkr.e7.model.Singleton;
+
+import java.util.Objects;
 
 public class RemoveAccountController extends Controller {
 
@@ -18,23 +21,28 @@ public class RemoveAccountController extends Controller {
     @FXML
     public void initialize() {
         Singleton.getInstance().addSceneHistory("view/RemoveAccount.fxml");
+        removeButton.setOnAction(this::remove);
     }
 
-    public void Remove(ActionEvent actionEvent) {
-        if (ssnField.getText().matches("^([0-9]{2})([0-9]{2})([0-9]{2})([a-zA-Z0-9][0-9]{3})$")) {
-            saveLabel.setText("");
-        }
-        if (!ssnField.getText().matches("^([0-9]{2})([0-9]{2})([0-9]{2})([a-zA-Z0-9][0-9]{3})$")) {
-            saveLabel.setText("input in YYMMDDXXXX form");
-        }
-        Employee employee = DatabaseHandler.load(Employee.class, ssnField.getText());
-
+    public void remove(ActionEvent actionEvent) {
         try {
-            DatabaseHandler.delete(employee);
+            if (!ssnField.getText().matches("^([0-9]{2})([0-9]{2})([0-9]{2})([a-zA-Z0-9][0-9]{3})$")) {
+                showError("Please input a valid SSN.");
+                return;
+            }
+
+            Employee employee = DatabaseHandler.load(Employee.class, ssnField.getText());
+            Patient patient = DatabaseHandler.load(Patient.class, ssnField.getText());
+
+            if (employee == null && patient == null) {
+                showError("No one with that SSN exists.");
+                return;
+            }
+
+            DatabaseHandler.delete(Objects.requireNonNullElse(employee, patient));
             showConfirmation("Deleted", "Done");
         } catch (Exception exception) {
-            showError("Did not delete, make sure you have right ssn");
-
+            showError("The account could not be deleted.");
         }
     }
 }
