e9dbb28262e6c7fbaaea35cf2ad34e5463139254
Willz <ekanemwills1@gmail.com>
2020-05-08 22:19:04 +0200

    Implented help menu button 1.

---
+++ src/main/java/se/hkr/e7/controller/HelpController.java
@@ -0,0 +1,13 @@
+package se.hkr.e7.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class HelpController {
+    public TextArea textArea;
+
+    @FXML
+    public void initialize(){
+
+    }
+}
