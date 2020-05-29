bbcdf2e3682dcb27f61acb5f5c6c21b7367cabc8
Willz <ekanemwills1@gmail.com>
2020-05-29 22:22:51 +0200

    Called createDefaultAdmin in main.

--- src/main/java/se/hkr/e7/Main.java
+++ src/main/java/se/hkr/e7/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        DatabaseHandler.reset(100);
+        DatabaseHandler.createDefaultAdmin();
         Controller.setStage(stage);
         URL resource = getClass().getClassLoader().getResource("view/Welcome.fxml");
         Parent root = FXMLLoader.load(resource);
