041adc17fb6917ba86c06cdf54bf52598b9d7f92
Hani311 <hani_adel.alzir0072@stud.hkr.se>
2020-05-29 17:06:48 +0200

    Send email result notification

--- src/main/java/se/hkr/e7/controller/AddResultController.java
+++ src/main/java/se/hkr/e7/controller/AddResultController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import se.hkr.e7.DatabaseHandler;
+import se.hkr.e7.Mail;
 import se.hkr.e7.Singleton;
 import se.hkr.e7.model.Employee;
 import se.hkr.e7.model.Patient;
@@ -94,6 +95,7 @@ public class AddResultController extends Controller {
             Result result = new Result(patient, currentUser, localDateTime, status);
             result.setNote(resultNote.getText());
             DatabaseHandler.save(result);
+            Mail.send("Result available", "Your results are now available \n Best Regards", patient);
             showConfirmation("Saved", "Thank you");
             ssnTextField.setText("");
             datePicker.setValue(null);
